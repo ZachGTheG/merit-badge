2b9685d3d7dd14b25e33586388ee2f5a162dd45b
Zach <113333499+ZachGTheG@users.noreply.github.com>
2023-04-13 12:56:00 -0400

    new installs and creating the initial badge framework

--- merit-badge/src/merit-badge.js
+++ merit-badge/src/merit-badge.js
@@ -1,88 +1,60 @@
-import { LitElement, html, css } from 'lit';
+import { LitElement, html, css } from "lit";
 
-const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
+const logo = new URL("../assets/open-wc-logo.svg", import.meta.url).href;
 
 class MeritBadge extends LitElement {
   static properties = {
     header: { type: String },
-  }
+  };
 
   static styles = css`
-    :host {
-      min-height: 100vh;
+    .container {
+      padding-top: 30px;
+      padding-left: 10px;
+      width: 220px;
+      height: 220px;
       display: flex;
-      flex-direction: column;
+      justify-content: center;
       align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
+    }
+
+    .badge {
+      width: 200px;
+      height: 200px;
+      background: red;
+      border-radius: 50%;
+      padding: 20px;
+      margin: 10px;
+      background: #ff0030;
+      color: #fff;
+      font-size: 21px;
+      font-weight: bold;
+      line-height: 1.3em;
+      border: 2px dashed #fff;
+      box-shadow: 0 0 0 4px #ff0030, 2px 1px 6px 4px rgba(10, 10, 0, 0.5);
+      text-shadow: -1px -1px #aa3030;
+      font-weight: normal;
       text-align: center;
-      background-color: var(--merit-badge-background-color);
-    }
-
-    main {
-      flex-grow: 1;
-    }
-
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
+      display: flex;
       align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
+      justify-content: center;
     }
   `;
 
   constructor() {
     super();
-    this.header = 'My app';
+    this.header = "My app";
   }
 
   render() {
     return html`
       <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.header}</h1>
-
-        <p>Edit <code>src/MeritBadge.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
+        <div class="container">
+          <div class="badge">Badge</div>
+        </div>
       </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
     `;
   }
 }
 
-customElements.define('merit-badge', MeritBadge);
+customElements.define("merit-badge", MeritBadge);
